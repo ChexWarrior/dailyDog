6b0463e55ecea3e5847dc355b5c74b24350be1ab
ChexWarrior <aplehm@gmail.com>
2019-03-31 09:07:09 -0400

    Add editorconfig and reformat files :mouse:

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
 const DOG_API_ENDPOINT = 'https://dog.ceo/api';
 
-const { tabs, alarms } = browser;
+const {tabs, alarms} = browser;
 
 let activeTabId;
 
-tabs.onActivated.addListener(tabInfo => ({ id: activeTabId } = tabInfo));
+tabs.onActivated.addListener(tabInfo => ({id: activeTabId} = tabInfo));
 
 alarms.create('test', {
-    periodInMinutes: 1.0,
+  periodInMinutes: 1.0,
 });
 
 alarms.onAlarm.addListener((alarmInfo) => {
-   tabs.executeScript(activeTabId, {
-       code: `console.log('Hey... Jiji, hows it goin?');`,
-   });
+  tabs.executeScript(activeTabId, {
+    code: `console.log('Hey... Jiji, hows it goin?');`,
+  });
 });
